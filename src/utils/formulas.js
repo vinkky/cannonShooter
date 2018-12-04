76cb5ef51fdfe3ee31bf20f14fe682ce932a67fa
Buda Tautvydas <tbuda@exadel.by>
2018-12-04 11:27:34 +0200

    added cannon rotation

--- src/utils/formulas.js
+++ src/utils/formulas.js
@@ -26,3 +26,16 @@ export const calculateAngle = (x1, y1, x2, y2) => {
   const quotient = dividend / divisor;
   return radiansToDegrees(Math.atan(quotient)) * -1;
 };
+
+export const getCanvasPosition = (event) => {
+ // mouse position on auto-scaling canvas
+ // https://stackoverflow.com/a/10298843/1232793
+
+ const svg = document.getElementById('aliens-go-home-canvas');
+ const point = svg.createSVGPoint();
+
+ point.x = event.clientX;
+ point.y = event.clientY;
+ const { x, y } = point.matrixTransform(svg.getScreenCTM().inverse());
+ return {x, y};
+};
